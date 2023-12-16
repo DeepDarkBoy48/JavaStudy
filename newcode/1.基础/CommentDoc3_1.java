7d64c974ffee031f0335b57e7fc28d6d6f6e1e74
AshBirdRobin <70373937+DeepDarkBoy48@users.noreply.github.com>
2023-12-16 19:15:34 +0000

    firstcommit

---
+++ newcode/1.基础/CommentDoc3_1.java
@@ -0,0 +1,14 @@
+/**
+ * @author 韩顺平
+ * @version 1.0
+ */
+public class CommentDoc3_1 {
+    // 编写一个 main 方法
+    public static void main(String[] args) {
+        // 选中，然后输入 tab 整体右移
+        // 选中，然后输入 shift+tab 整体左移
+        int n1 = 1 + 2;
+        int n2 = 5 + 10;
+        int n3 = 1 + 3 * 34;
+    }
+}
---
+++ newcode/chapter1/ChangeCharExer2_1.java
@@ -0,0 +1,16 @@
+public class ChangeCharExer2_1 {
+    // 编写一个 main 方法
+    public static void main(String[] args) {
+        // 完成代码
+        System.out.println("书名\t 作者\t 价格\t 销量\n 三国\t 罗贯中\t120\t1000");
+        // 示意 => 可读性很好
+        // 下面代码完成 两个数相加
+        // 定义变量
+        int n1 = 10;
+        int n2 = 30;
+        // 求和
+        int sum = n1 + n2;
+        // 输出结果
+        System.out.println("结果=" + sum);
+    }
+}
